0ed3fea4e6b772dc216b470970cab4bd68cf3262
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2023-01-26 22:30:43 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int result = 0;
+
+        while (n > 0) {
+            result += n % 10;
+            n /= 10;
+        }
+        return result;
+    }
+}
