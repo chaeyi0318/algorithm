2b35766ef7c7187be34c11d01055b1aa0f2dfde6
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2023-01-27 10:08:45 +0900

    [level 1] Title: 2016년, Time: 18.69 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -0,0 +1,37 @@
+import java.util.Calendar;
+
+class Solution {
+    public String solution(int a, int b) {
+        String day = "";
+        
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2016, a - 1, b);
+        
+        int dayNum = calendar.get(Calendar.DAY_OF_WEEK);
+
+        switch (dayNum) {
+            case 1:
+                day = "SUN";
+                break;
+            case 2:
+                day = "MON";
+                break;
+            case 3:
+                day = "TUE";
+                break;
+            case 4:
+                day = "WED";
+                break;
+            case 5:
+                day = "THU";
+                break;
+            case 6:
+                day = "FRI";
+                break;
+            case 7:
+                day = "SAT";
+                break;
+        }
+        return day;
+    }
+}
