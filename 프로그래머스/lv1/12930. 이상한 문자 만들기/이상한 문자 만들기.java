e6a9f88df2607ea38237fe85a6ff6ad55a6680ba
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2023-01-27 16:19:19 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 2.29 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int cnt = 0;
+        String[] array = s.split("");
+
+        for(String ss : array) {
+            cnt = ss.contains(" ") ? 0 : cnt + 1;
+            answer += cnt%2 == 0 ? ss.toLowerCase() : ss.toUpperCase(); 
+        }
+      return answer;
+    }
+}
