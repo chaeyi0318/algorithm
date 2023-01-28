7f507309f0611c18ce02e591dda8dc1a3c5e56a0
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2023-01-29 00:04:13 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.10 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,26 +1,27 @@
 class Solution {
     public int[] solution(int[] arr) {
-        if(arr.length == 1) {
+        if(arr.length == 1){
             int[] answer = {-1};
             return answer;
         }
 
-        int[] answer = new int[arr.length -1];
-
+        int[] answer = new int[arr.length-1];
+        
         int min = arr[0];
 
-        // int index = 0;
-        
-        for (int i = 0; i < arr.length; i++) {
+        for(int i=1; i<arr.length; i++){
             min = Math.min(min, arr[i]);
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            if(min == arr[i]) {
+        int index = 0;
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == min){
                 continue;
-            } 
-            answer[i] = arr[i];
+            }
+            answer[index++] = arr[i];
         }
+        
         return answer;
     }
 }
