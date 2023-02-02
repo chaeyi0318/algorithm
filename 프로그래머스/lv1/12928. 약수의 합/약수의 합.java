742b43e2c50e55ce819f19765c8feca295a266b2
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2023-02-03 00:59:20 +0900

    [level 1] Title: 약수의 합, Time: 0.08 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -4,7 +4,7 @@ class Solution {
         
         for(int i = 1; i <= n; i++) {
             if(n % i == 0) {
-                answer += n / i;
+                answer += i;
             }
         }
         return answer;
