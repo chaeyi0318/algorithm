977be8d6f164dec772b9ac3795b0e2f5b05b5e13
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2023-03-23 01:14:21 +0900

    [Bronze I] Title: 단어 공부, Time: 260 ms, Memory: 20072 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] arr = new int[26];
+		String s = br.readLine();
+		
+		for (int i = 0; i < s.length(); i++) {
+			if ('a' <= s.charAt(i) && s.charAt(i) <= 'z') {
+				arr[s.charAt(i) - 97]++;
+			} else {
+                arr[s.charAt(i) - 65]++;
+            }
+		}
+        
+		int max = -1;
+		char ch = '?';
+        
+		for (int i = 0; i < 26; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+				ch = (char) (i + 65);
+			} 
+			else if (arr[i] == max) {
+				ch = '?';
+			}
+		}
+		System.out.print(ch);
+	}
+}
