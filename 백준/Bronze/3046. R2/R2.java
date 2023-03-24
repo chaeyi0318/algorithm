afa94e8b18f54b753c58e70f8f786454595f049f
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2023-03-24 20:30:32 +0900

    [Bronze IV] Title: R2, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int r1 = sc.nextInt();
+        int s = sc.nextInt();
+
+        System.out.println((s * 2) - r1);
+    }
+}
