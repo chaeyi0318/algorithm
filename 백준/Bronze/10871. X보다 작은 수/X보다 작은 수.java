39e0be54e738acad6e532160f066b8b8f87c6c16
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2023-03-27 23:30:05 +0900

    [Bronze V] Title: X보다 작은 수, Time: 628 ms, Memory: 30528 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
-public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int x = sc.nextInt();
-		
-		for(int i = 0; i < n; i++)
-		{
-			int j = sc.nextInt();
-			if(j < x)
-			{
-				System.out.print(j + " ");
-			}
-		}
-		sc.close();
-	}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < x) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
 }
