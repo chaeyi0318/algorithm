0d5f62440b4a65b96134264ab633f54d2bd32405
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2023-05-12 00:56:37 +0900

    [unrated] Title: 이어 붙인 수, Time: 12.66 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String odd = "";
+        String even = "";
+        
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] % 2 == 0) {
+                even += num_list[i];
+            } else {
+                odd += num_list[i];
+            }
+        }
+        
+        answer = Integer.parseInt(even) + Integer.parseInt(odd);
+        
+        return answer;
+    }
+}
