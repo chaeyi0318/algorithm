31ad17a6de4fd2b9b86a87d269e86d1e0081a4a2
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2023-05-16 21:32:18 +0900

    [unrated] Title: 뒤에서 5등 위로, Time: 0.52 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int [num_list.length - 5];
+        
+        Arrays.sort(num_list);
+        
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = num_list[i + 5];
+        }
+        
+        return answer;
+    }
+}
