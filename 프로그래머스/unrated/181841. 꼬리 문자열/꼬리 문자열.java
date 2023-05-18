677c6dc42e6a0f320844ef751f67334706dbb1d9
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2023-05-19 01:31:17 +0900

    [unrated] Title: 꼬리 문자열, Time: 1.16 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        String answer = "";
+        
+        for (int i = 0; i < str_list.length; i++) {
+            if (str_list[i].contains(ex)) {
+                continue;
+            } else {
+                answer += str_list[i];
+            }
+        }
+        return answer;
+    }
+}
