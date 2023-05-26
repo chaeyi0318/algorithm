8420f5af0fd1a9bcc714d1b27e9f522f10806ea0
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2023-05-26 21:01:46 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 240 ms, Memory: 17308 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger num1 = new BigInteger(st.nextToken());
+        BigInteger num2 = new BigInteger(st.nextToken());
+
+        System.out.println(num1.add(num2));
+    }
+}
