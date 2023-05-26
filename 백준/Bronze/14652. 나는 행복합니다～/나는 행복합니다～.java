c3e4c9cacd44508bb8263a0bd8f36b9208231d4f
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2023-05-26 21:14:04 +0900

    [Bronze V] Title: 나는 행복합니다~, Time: 152 ms, Memory: 16036 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(k / m + " " + k % m);
+        bw.close();
+    }
+}
