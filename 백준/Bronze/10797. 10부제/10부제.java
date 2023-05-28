04ef8b38d3f3f9b15533d3a23e2339288a3202fb
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2023-05-29 01:03:08 +0900

    [Bronze IV] Title: 10부제, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/10797. 10부제/10부제.java
+++ 백준/Bronze/10797. 10부제/10부제.java
@@ -4,6 +4,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int date = Integer.parseInt(br.readLine());
 
@@ -16,6 +17,7 @@ public class Main {
                 count++;
             }
         }
-        System.out.println(count);
+        bw.write(String.valueOf(count));
+        bw.close();
     }
 }
