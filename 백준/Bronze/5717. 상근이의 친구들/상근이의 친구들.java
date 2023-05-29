131a0ef5ee22c7b574064b5edbe735e3a31a6f6e
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2023-05-29 15:23:29 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 116 ms, Memory: 14036 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+
+            if (num1 == 0 && num2 == 0) {
+                break;
+            }
+            System.out.println(num1 + num2);
+        }
+    }
+}
