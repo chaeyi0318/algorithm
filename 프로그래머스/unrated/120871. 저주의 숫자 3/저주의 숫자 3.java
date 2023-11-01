a74fdd76e667ce968f92bd18055e4c7328167e63
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2023-11-01 23:38:53 +0900

    [level unrated] Title: 저주의 숫자 3, Time: 0.24 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            answer++;
+            
+            while (String.valueOf(answer).contains("3") || answer % 3 == 0) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
