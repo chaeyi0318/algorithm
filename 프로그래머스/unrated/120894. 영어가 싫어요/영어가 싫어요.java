b30f79e57114f39f0256321e2bcb4dd0b573ca47
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2023-11-01 23:20:16 +0900

    [level unrated] Title: 영어가 싫어요, Time: 0.11 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long solution(String numbers) {
+        long answer = 0;
+
+        answer = Long.parseLong(numbers.replace("one", "1")
+                              .replace("two", "2")
+                              .replace("three", "3")
+                              .replace("four", "4")
+                              .replace("five", "5")
+                              .replace("six", "6")
+                              .replace("seven", "7")
+                              .replace("eight", "8")
+                              .replace("nine", "9")
+                              .replace("zero", "0"));
+
+        return answer;
+    }
+}
