5640f04112b38cd040b5060aa728a5d05cdcdd42
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2024-06-18 19:59:57 +0900

    [D1] Title: 홀수만 더하기, Time: 139 ms, Memory: 23,984 KB -BaekjoonHub

---
+++ SWEA/D1/2072. 홀수만 더하기/README.md
@@ -0,0 +1,15 @@
+# [D1] 홀수만 더하기 - 2072 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QSEhaA5sDFAUq) 
+
+### 성능 요약
+
+메모리: 23,984 KB, 시간: 139 ms, 코드길이: 2,469 Bytes
+
+### 제출 일자
+
+2024-06-18 19:59
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.java
@@ -0,0 +1,64 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= num; i++) {
+            int sum = 0;
+            String str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str, " ");
+
+            while (st.hasMoreTokens()) {
+                int num2 = Integer.parseInt(st.nextToken());
+
+                if (num2 % 2 != 0) {
+                    sum += num2;
+                }
+            }
+            bw.write("#" + i + " " + sum + "\n");
+        }
+        br.close();
+        bw.close();
+	}
+}
