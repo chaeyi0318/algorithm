e12f2bb6efbecb4e65bf9dd672ac600775e0166c
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2024-07-03 00:03:13 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.69 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,23 +1,29 @@
+import java.util.List;
+import java.util.ArrayList;
+
 class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
-
+        
+        List<Integer> list = new ArrayList<>();
+        
         for (int i = 1; i <= number; i++) {
             int count = 0;
-
-            for (int j = 1; j <= Math.sqrt(i); j++) {
-                if (j * j == i) {
+            
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
                     count++;
-                } else if (i % j == 0) {
-                    count += 2;
                 }
             }
-            if (count > limit) {
-                count = power;
+            list.add(count);
+            
+            if (list.get(i - 1) > limit) {
+                answer += power;
+            } else {
+                answer += list.get(i - 1);
             }
-            answer += count;
         }
-
+        
         return answer;
     }
 }
