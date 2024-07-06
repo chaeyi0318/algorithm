8473246b43c9d6f12829dd63dfe26e0b06b3a760
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2024-07-06 18:05:43 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.05 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -1,7 +1,2 @@
-def solution(numbers):
-    answer = []
-    
-    for i in numbers:
-        answer.append(i * 2)
-    
-    return answer
+def solution(numbers):   
+    return [i * 2 for i in numbers]
