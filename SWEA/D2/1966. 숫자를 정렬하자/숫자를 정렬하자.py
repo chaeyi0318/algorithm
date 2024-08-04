5bc30d92fe6f917b1b4aafd00d7eda9f58a2b477
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2024-08-05 01:58:18 +0900

    [D2] Title: 숫자를 정렬하자, Time: 112 ms, Memory: 44,796 KB -BaekjoonHub

--- SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.py
+++ SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.py
@@ -1,10 +1,12 @@
 T = int(input())
 
-for i in range(1, T + 1):
+for t in range(1, T + 1):
     N = int(input())
+    arr = list(map(int, input().split()))
 
-    li = list(map(int, input().split()))
+    for i in range(len(arr) - 1, 0, -1):
+        for j in range(i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-    li.sort()
-
-    print(f"#{i}", *li)
+    print(f'#{t}', *arr)
