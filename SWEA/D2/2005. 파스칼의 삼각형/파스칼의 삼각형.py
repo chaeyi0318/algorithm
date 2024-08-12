daf8d3b033de3d5487f616b2664e1d2e7d40f484
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2024-08-12 23:14:37 +0900

    [D2] Title: 파스칼의 삼각형, Time: 129 ms, Memory: 47,880 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -1,26 +1,22 @@
+# 재귀함수
 T = int(input())
 
-def triangle(N):
-    arr = [[0] * N for _ in range(N)]
-    arr[0][0] = 1
+def pascal_triangle(i, j):
+    if i == j or j == 0:
+        return 1
+    return pascal_triangle(i - 1, j - 1) + pascal_triangle(i - 1, j)
 
-    for i in range(1, len(arr)):
-        for j in range(len(arr[i])):
-            if i - 1 < 0:
-                arr[i][j] = 1
-            else:
-                arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j]
-    return arr
-
-for t in range(1, T + 1):
+for t in range(1, 1 + T):
     N = int(input())
+    triangle = []
 
-    result = triangle(N)
+    for i in range(N):
+        row = []
+        for j in range(i + 1):
+            row.append(pascal_triangle(i, j))
+        triangle.append(row)
 
     print(f'#{t}')
-
-    for i in range(len(result)):
-        for j in range(len(result[i])):
-            if result[i][j] != 0:
-                print(result[i][j], end=' ')
-        print()
+    
+    for i in range(len(triangle)):
+        print(*triangle[i])
