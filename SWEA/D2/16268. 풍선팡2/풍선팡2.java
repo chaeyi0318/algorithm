d6d33500b89e9097cfd7e9479cf224cf1ecbce71
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2024-09-12 17:04:12 +0900

    [D2] Title: 풍선팡2, Time: 153 ms, Memory: 24,904 KB -BaekjoonHub

--- SWEA/D2/16268. 풍선팡2/풍선팡2.java
+++ SWEA/D2/16268. 풍선팡2/풍선팡2.java
@@ -1,42 +1,55 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-class Solution {
-    public static void main(String args[]) throws Exception {
-        int[][] dxy = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T = sc.nextInt();
-
-        for (int test_case = 1; test_case <= T; test_case++) {
-            int N = sc.nextInt();
-            int M = sc.nextInt();
-            int[][] board = new int[N][M];
-
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    board[i][j] = sc.nextInt();
-                }
-            }
-
-            int max_value = 0;
-
-            for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board[i].length; j++) {
-                    int cnt = board[i][j];
-
-                    for (int k = 0; k < dxy.length; k++) {
-                        int point_x = i + dxy[k][0];
-                        int point_y = j + dxy[k][1];
-
-                        if (point_x >= 0 && point_x < N && point_y >= 0 && point_y < M) {
-                            cnt += board[point_x][point_y];
-                        }
-                    }
-                    max_value = cnt > max_value ? cnt : max_value;
-                }
-            }
-            System.out.println("#" + test_case + " " + max_value);
-        }
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	
+    	int[][] dxy = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    	
+    	int T = Integer.parseInt(br.readLine());
+    	
+    	for (int t = 1; t <= T; t++) {
+    		StringTokenizer st = new StringTokenizer(br.readLine());
+    		
+    		int N = Integer.parseInt(st.nextToken());
+    		int M = Integer.parseInt(st.nextToken());
+    		
+    		int[][] board = new int[N][M];
+    		
+    		for (int i = 0; i < board.length; i++) {
+    			st = new StringTokenizer(br.readLine());
+    			for (int j = 0; j < board[0].length; j++) {
+    				board[i][j] = Integer.parseInt(st.nextToken());
+    			}
+    		}
+    		
+    		int result = 0;
+    		
+    		for (int i = 0; i < N; i++) {
+    			for (int j = 0; j < M; j++) {
+    				int cnt = board[i][j];
+    				for (int d = 0; d < 4; d++) {
+    					int nx = i + dxy[d][0];
+    					int ny = j + dxy[d][1];
+    					
+    					if (0 <= nx && nx < N && 0 <= ny && ny < M) {
+    						cnt += board[nx][ny];
+    					}
+    				}
+    				if (result < cnt) {
+    					result = cnt;
+    				}
+    			}
+    		}
+    		bw.write("#" + t + " " + result + "\n");
+    	}
+    	bw.close();
     }
 }
