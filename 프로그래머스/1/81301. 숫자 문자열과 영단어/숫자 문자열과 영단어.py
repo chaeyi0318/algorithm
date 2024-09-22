96219af440e1c7714a60e1f1dd8b66e122fd4b74
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2024-09-22 23:18:55 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,20 @@
+def solution(s):
+    answer = s
+
+    num_dict = {
+        'zero': '0',
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9',
+    }
+
+    for key, value in num_dict.items():
+        answer = answer.replace(key, value)
+
+    return int(answer)
