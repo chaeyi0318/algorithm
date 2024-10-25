1f92ff17992325420dd730828bcf83a5746a65ee
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2024-10-25 12:18:00 +0900

    [Silver V] Title: 숫자 카드, Time: 928 ms, Memory: 139884 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,46 +1,25 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static int binarySearch (int element, int[] num) {
-		int left = 0;
-		int right = num.length - 1;
-		
-		while (left <= right) {
-			int mid = left + (right - left) / 2;
-			
-			if (num[mid] == element) {
-				return 1;
-			} else if (num[mid] < element) {
-				left = mid + 1;
-			} else {
-				right = mid - 1;
-			}
-		}
-		
-		return 0;
-	}
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
-		int[] card = new int[N];
+		HashSet<Integer> cardSet = new HashSet<>();
 		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		for (int i = 0; i < N; i++) {
-			card[i] = Integer.parseInt(st.nextToken());
+			cardSet.add(Integer.parseInt(st.nextToken()));
 		}
-		
-		Arrays.sort(card);
 		
 		int M = Integer.parseInt(br.readLine());
 		int[] numArr = new int[M];
 		
-		st = new StringTokenizer(br.readLine(), " ");
+		st = new StringTokenizer(br.readLine());
 		
 		for (int i = 0; i < M; i++) {
 			numArr[i] = Integer.parseInt(st.nextToken());
@@ -49,7 +28,11 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		
 		for (int i = 0; i < M; i++) {
-			sb.append(binarySearch(numArr[i], card) + " ");
+			if (cardSet.contains(numArr[i])) {
+				sb.append(1).append(" ");
+			} else {
+				sb.append(0).append(" ");
+			}
 		}
 		
 		System.out.println(sb.toString());
