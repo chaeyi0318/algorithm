38e62194cd647cbfde3cb543486d1fe08ed35359
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2024-11-04 23:43:31 +0900

    [Silver II] Title: 최대 힙, Time: 288 ms, Memory: 26476 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(Collections.reverseOrder());
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x == 0) {
+                sb.append(minHeap.isEmpty() ? 0 : minHeap.poll()).append("\n");
+            } else {
+                minHeap.offer(x);
+            }
+        }
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
