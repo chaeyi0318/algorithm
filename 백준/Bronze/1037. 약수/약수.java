5a9ed980b3038c9637abbc550a15b0d3cd6fea01
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2025-01-20 23:45:01 +0900

    [Bronze I] Title: 약수, Time: 104 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        System.out.print(arr[0] * arr[n - 1]);
+    }
+}
