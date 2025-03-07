024ebf51ef65b6a04a2ffb37b8df5d5c6ec6d16d
chaeyi <95565436+chaeyi0318@users.noreply.github.com>
2025-03-07 10:13:58 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,21 +1,25 @@
+WITH RECURSIVE hierarchy AS (
+    SELECT
+        ID, PARENT_ID, 1 AS generation
+    FROM
+        ECOLI_DATA
+    WHERE
+        PARENT_ID IS NULL
+    
+    UNION ALL
+    
+    SELECT
+        e.ID, e.PARENT_ID, h.generation + 1
+    FROM
+        ECOLI_DATA e JOIN hierarchy h ON e.PARENT_ID = h.ID
+    WHERE
+        h.generation < 3
+)
 SELECT
     ID
-FROM 
-    ECOLI_DATA
+FROM
+    hierarchy
 WHERE
-    PARENT_ID IN (
-        SELECT
-            ID
-        FROM 
-            ECOLI_DATA
-        WHERE
-            PARENT_ID IN (
-                SELECT
-                    ID
-                FROM
-                  ECOLI_DATA
-                WHERE PARENT_ID IS NULL
-            )
-    )
+    generation = 3
 ORDER BY
     ID;
